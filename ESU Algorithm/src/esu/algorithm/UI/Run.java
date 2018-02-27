5df1bd1b3c5109102719c55a99110895bcb51c09
Dawit Abera <dawitabera64@gmail.com>
2018-02-27 14:05:55 -0800

    - Splash screen added

---
+++ ESU Algorithm/src/esu/algorithm/UI/Run.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esu.algorithm.UI;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author dawitabera
+ */
+public class Run extends Application{
+    ESUDriver open = new ESUDriver();
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("splashScreen.fxml"));
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(5),root);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.setCycleCount(1);
+        
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(3000),root);
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+        fadeOut.setCycleCount(1);
+        
+        fadeIn.play();
+        
+        fadeIn.setOnFinished((event)->{
+            fadeOut.play();
+            open.start(stage);
+        });
+        Scene scene = new Scene(root);
+        stage.setTitle("Undirected Subgraph Enumeration Software");
+        stage.setScene(scene);
+        stage.show();
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
