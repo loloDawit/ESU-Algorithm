152ff3bcab3eda698ae1d071285e2de60db25dc1
Dawit Abera <dawitabera64@gmail.com>
2018-02-28 11:43:03 -0800

    - updated this class to create a Node Object for the Graph

--- ESU Algorithm/src/esu/algorithm/UI/DrawNode.java
+++ ESU Algorithm/src/esu/algorithm/UI/DrawNode.java
@@ -3,38 +3,49 @@
  */
 package esu.algorithm.UI;
 
-import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.control.Label;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.text.Font;
-import javafx.scene.text.TextAlignment;
-
 /**
- *
+ * This class is used to create a NodeObject for the Graph  
+ * 
  * @author BioHazard
  */
-public class DrawNode extends Group{
-    int r,g,b = (int)(Math.random()*256);
-    int raduis = 30;
-    final Font font = new Font("ubnutu", 17);
+public class DrawNode {
+    
+    private final char    nodeName;
+    private final int     locationX;
+    private final int     locationY;
     /**
+     * Default constructor
      * 
-     * @param data 
+     * @param newLocationX  location on the x axis
+     * @param newLocationY  location on the y axis
+     * @param name          character identifier of the node
      */
-    public DrawNode(int data){
-        Circle c = new Circle(raduis,Color.rgb(r, g, b)) ;
-	Label nodeDataContent = new Label(data+"");
-	nodeDataContent.setTextFill(Color.WHITE);
-	nodeDataContent.setFont(font);
-	nodeDataContent.setPrefSize(raduis*2, raduis*2);
-	nodeDataContent.setTranslateX(-raduis);
-	nodeDataContent.setTranslateY(-raduis);
-	nodeDataContent.setTextAlignment(TextAlignment.CENTER);
-	nodeDataContent.setAlignment(Pos.CENTER);
-	getChildren().addAll(c,nodeDataContent);
+    public DrawNode(int newLocationX, int newLocationY, char gname){
+        this.nodeName    = gname;
+        this.locationX   = newLocationX;
+        this.locationY   = newLocationY;
     }
     
+    /**
+     * Getter for location on the x axis
+     * @return integer locationX
+     */
+    public int getLocationX() {
+        return  locationX;
+    }
+    /**
+     * Getter for location on the y axis
+     * @return integer locationY
+     */    
+    public int getLocationY() {
+        return  locationY;
+    }
+    /**
+     * Getter for the character name of the node
+     * @return character nodeName
+     */    
+    public char getNodeName() {
+        return  nodeName;
+    }
     
 }
