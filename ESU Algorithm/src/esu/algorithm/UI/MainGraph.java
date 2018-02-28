a13d7a0fe6d5dd9fa11401f5b55d74b182f50084
Dawit Abera <dawitabera64@gmail.com>
2018-02-28 12:42:26 -0800

    - main method added to run the undirected graph

---
+++ ESU Algorithm/src/esu/algorithm/UI/MainGraph.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esu.algorithm.UI;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author dawitabera
+ */
+public class MainGraph extends Application {
+     @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GraphApp.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
