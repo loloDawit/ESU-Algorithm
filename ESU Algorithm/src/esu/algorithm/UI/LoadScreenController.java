2b0c62fe07aa3774a30f6133586a7a6cdf8c0952
Dawit Abera <dawitabera64@gmail.com>
2018-03-02 10:14:13 -0800

    - Now we can open the the project document from the application. - we can open any pdf file

--- ESU Algorithm/src/esu/algorithm/UI/LoadScreenController.java
+++ ESU Algorithm/src/esu/algorithm/UI/LoadScreenController.java
@@ -5,12 +5,17 @@
  */
 package esu.algorithm.UI;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.HostServices;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -46,12 +51,34 @@ public class LoadScreenController implements Initializable {
     }
 
     @FXML
-    private void showDoc(ActionEvent event) {
+    private void showDoc(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf");
+        fileChooser.getExtensionFilters().add(extFilter);
+              
+        //Show open file dialog
+        File file = fileChooser.showOpenDialog(new Stage());
+        if(file.exists()){
+           if(Desktop.isDesktopSupported()){
+               Desktop.getDesktop().open(file);
+           }
+        }else
+            Alerts.displayFileNotFound();
     }
 
     @FXML
     private void showTeam(ActionEvent event) {
         System.out.println("dawit");
+    }
+    private static void configureFileChooser(final FileChooser fileChooser){                           
+        fileChooser.setTitle("View Graph Files");
+        fileChooser.setInitialDirectory(
+            new File(System.getProperty("user.home"))
+        ); 
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("PDF files (*.pdf)\", \"*.pdf"));
+        
     }
         
     
