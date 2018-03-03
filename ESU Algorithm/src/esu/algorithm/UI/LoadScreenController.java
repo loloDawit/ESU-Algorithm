b1ca0ea9199e622871c35dbe948fd416275e0c7c
Dawit Abera <dawitabera64@gmail.com>
2018-03-03 15:29:53 -0800

    - show team members implemented

--- ESU Algorithm/src/esu/algorithm/UI/LoadScreenController.java
+++ ESU Algorithm/src/esu/algorithm/UI/LoadScreenController.java
@@ -10,13 +10,18 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.application.HostServices;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -76,7 +81,8 @@ public class LoadScreenController implements Initializable {
 
     @FXML
     private void showTeam(ActionEvent event) {
-        System.out.println("dawit");
+        loadWindow("/esu/algorithm/UI/showTeam.fxml", "Biohazard Team Members");
+        
     }
     private static void configureFileChooser(final FileChooser fileChooser){                           
         fileChooser.setTitle("View Graph Files");
@@ -90,6 +96,22 @@ public class LoadScreenController implements Initializable {
     public void closeStage() {
         ((Stage)checkUniGraph.getScene().getWindow()).close();
     }
+    /**
+     * 
+     * @param loc
+     * @param title 
+     */
+    void loadWindow(String loc, String title){
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource(loc));
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setTitle(title);
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(LoadScreenController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
+    }  
     
 }
